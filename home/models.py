be623b79495c2aaf4b9d5d414baa2f32155c1d3c
alexmais95 <112903498+alexmais95@users.noreply.github.com>
2023-07-20 23:03:52 +0300

    add models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Coffee(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    photo = models.ImageField()
+    cat = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
